f3eef2cccbddd129dc2db053aa2ffe3672cedfbe
Breyten Ernsting <bje@dds.nl>
2016-09-09 14:20:27 +0200

    Play with css styling.

--- os2pdf.py
+++ os2pdf.py
@@ -8,8 +8,19 @@ import requests
 import pdfkit
 
 def main():
+    options = {
+        #'quiet': '',
+        'user-style-sheet': 'os2pdf.css',
+        'page-size': 'A4',
+        'margin-top': '0.75in',
+        'margin-right': '0.75in',
+        'margin-bottom': '0.75in',
+        'margin-left': '0.75in',
+        'encoding': "UTF-8",
+        'no-outline': None
+    }
     url = 'http://www.openspending.nl/utrecht-gemeente/realisatie/2016/lasten/hoofdfuncties/'
-    pdfkit.from_url(url, 'utrecht.pdf')
+    pdfkit.from_url(url, 'utrecht.pdf', options=options)
 
 if __name__ == '__main__':
     main()
